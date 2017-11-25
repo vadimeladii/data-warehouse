c8532b497f6c75fbee601130e73483628182dca8
vadimeladii <vadimeladii@gmail.ru>
2017-11-25 18:23:41 +0200

    added script task for travis.yml

--- webservice/src/main/java/md/utm/fcim/webservice/config/spring/core/DataWarehouseApplication.java
+++ webservice/src/main/java/md/utm/fcim/webservice/config/spring/core/DataWarehouseApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "md.utm.fcim")
-@EntityScan("md.utm.fcim")
-@EnableJpaRepositories("md.utm.fcim")
+@SpringBootApplication(scanBasePackages = {"md.utm.fcim.repository", "md.utm.fcim.service", "md.utm.fcim.webservice"})
+@EntityScan({"md.utm.fcim.repository", "md.utm.fcim.service", "md.utm.fcim.webservice"})
+@EnableJpaRepositories({"md.utm.fcim.repository", "md.utm.fcim.service", "md.utm.fcim.webservice"})
 public class DataWarehouseApplication {
 
 	public static void main(String[] args) {
