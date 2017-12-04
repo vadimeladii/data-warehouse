9be0de871a56ee070470056f5c36f99f4702a0ac
vadimeladii <vadimeladii@gmail.ru>
2017-12-05 00:39:34 +0200

    Fixed swagger config for docker

--- webservice/src/main/java/md/utm/fcim/webservice/config/spring/jersey/JerseyConfig.java
+++ webservice/src/main/java/md/utm/fcim/webservice/config/spring/jersey/JerseyConfig.java
@@ -1,9 +1,8 @@
 package md.utm.fcim.webservice.config.spring.jersey;
 
 import io.swagger.jaxrs.config.BeanConfig;
-import io.swagger.jaxrs.config.SwaggerConfigLocator;
-import io.swagger.jaxrs.config.SwaggerContextService;
 import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
 import md.utm.fcim.common.error.config.DuplicateEntityExceptionMapper;
 import md.utm.fcim.common.error.config.EntityNotFoundExceptionMapper;
 import md.utm.fcim.common.error.config.IllegalArgumentExceptionMapper;
@@ -13,6 +12,7 @@ import md.utm.fcim.webservice.controller.impl.BookControllerImpl;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.ws.rs.ApplicationPath;
 
 @Component
@@ -21,12 +21,12 @@ public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
         registerEndpoints();
-//        BeanConfig swaggerConfig = new BeanConfig();
-//        swaggerConfig.setBasePath("/api");
-//        SwaggerConfigLocator.getInstance().putConfig(SwaggerContextService.CONFIG_ID_DEFAULT, swaggerConfig);
-//
-//        packages(getClass().getPackage().getName(), ApiListingResource.class.getPackage().getName());
+    }
 
+    @PostConstruct
+    public void init() {
+        // Register components where DI is needed
+        this.configureSwagger();
     }
 
     private void registerEndpoints() {
@@ -36,5 +36,22 @@ public class JerseyConfig extends ResourceConfig {
         register(WrongUrlIdExceptionMapper.class);
         register(DuplicateEntityExceptionMapper.class);
         register(IllegalArgumentExceptionMapper.class);
+    }
+
+    private void configureSwagger() {
+        // Available at localhost:port/swagger.json
+        this.register(ApiListingResource.class);
+        this.register(SwaggerSerializers.class);
+
+        BeanConfig config = new BeanConfig();
+        config.setConfigId("springboot-jersey-swagger-docker-example");
+        config.setTitle("Data WareHouse");
+        config.setVersion("v1");
+        config.setContact("Eladii Vadim");
+        config.setSchemes(new String[] { "http", "https" });
+        config.setResourcePackage("md.utm.fcim.webservice");
+        config.setPrettyPrint(true);
+        config.setBasePath("/api");
+        config.setScan(true);
     }
 }
